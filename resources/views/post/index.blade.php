ff85f970ecd40f70c988cee1106280cc21093451
sys72740hro <Hcbdag@635>
2022-06-20 15:33:21 +0200

    user afhankelijkheid van posten gemaakt.

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -43,12 +43,15 @@
 
                                 <a class="btn btn-info btn-sm text-white" href="{{ route('posts.show',$post->id) }}">Show</a>
 
-                                <a class="btn btn-primary btn-sm" href="{{ route('posts.edit',$post->id) }}">Edit</a>
+                                @if ($post->user_id == auth()->user()->getAuthIdentifier())
+                                    <a class="btn btn-primary btn-sm"
+                                       href="{{ route('posts.edit',$post->id) }}">Edit</a>
 
-                                @csrf
-                                @method('DELETE')
+                                    @csrf
+                                    @method('DELETE')
 
-                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                @endif
                             </form>
                         </td>
                     </tr>
