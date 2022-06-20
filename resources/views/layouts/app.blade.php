937dbd700df15c6731823e716c424eb9fd5ce0eb
sys72740hro <Hcbdag@635>
2022-06-20 15:03:16 +0200

    menu post verwijderd.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,6 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'ImkerApp') }}
                 </a>
-                <a class="navbar-brand" href="{{ url('/posts') }}">
-                    Posts
-                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
